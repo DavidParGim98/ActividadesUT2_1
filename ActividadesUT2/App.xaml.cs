b51bebae6f6e80f22be880adb4d681d931f46332
David Pardines <david.pardinesgim@iesdoctorbalmis.com>
2020-10-27 17:36:49 +0100

    Agregar archivos de proyecto.

---
+++ ActividadesUT2/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Castle.Components.DictionaryAdapter.Xml;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+using DocumentFormat.OpenXml.Spreadsheet;
+using LanguageExt.ClassInstances;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ActividadesUT2
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+}
---
+++ ActividadesUT2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActividadesUT2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Random random = new Random();
+        int numeroGanador = random.Next(1 ,100);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        void miBoton_Comprobar(object sender, RoutedEventArgs e)
+        {
+            if (TextoComprobar.Text.Length==0)
+            {
+                Pista.Text = "Introduce un numero";
+            }
+            else if (numeroGanador == int.Parse(TextoComprobar.Text))
+            {
+                Pista.Text="Lo has acertado";
+            }
+            else if(numeroGanador < int.Parse(TextoComprobar.Text))
+            {
+                Pista.Text = "Prueba un número mas pequeño";
+            }
+            else if (numeroGanador > int.Parse(TextoComprobar.Text))
+            {
+                Pista.Text = "Prueba un número mas grande";
+            }
+            else
+            {
+                Pista.Text = "Has acertado el número";
+            }
+
+        }
+
+        void miBoton_Reiniciar(object sender, RoutedEventArgs e)
+        {
+            numeroGanador = random.Next(1, 100);
+            Pista.Text = "";
+            TextoComprobar.Clear();
+        }
+
+    }
+}
